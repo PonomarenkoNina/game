eb24de8bb274bc6b6f9dbf6546ab99a362fbb7ad
PonomarenkoNina <ninulka@ukr.net>
2024-02-01 22:23:55 +0200

    fixing all the code

--- game.js
+++ game.js
@@ -2,52 +2,54 @@
 var cities = ["Київ", "Вінниця", "Яготин", "Нова Каховка", "Авдіївка"];
 // Definition of variables - Визначення змінних
 var attempt;
-var correct = 0;
-var attempts = 0;
-var weFinished = false;
-
-// The appearance of a dialog box with a term, where the user will enter the name of the city - Поява діалогового вікна із строкою, куди користувач буде вводити назву міста
-var attempt = prompt("Починаємо гру в города! (Введіть назву міста):");
-alert(attempt);
-
-// Calling the function and checking the correctness of entering the city name - виклик функції та перевірка правильності введеня назви міста
+var wrongAttempts = 0; // Змінна для підрахунку помилок
+// User input of a letter in a dialog box - Введення користувачем міста в діалогове вікно
+attempt = prompt("Починаємо гру! (введіть, будь ласка назву міста):");
+// Виклик функції та отримання міста, яке ввів користувач
+// запам'ятовує назву міста і на яку літеру закінчується, знаходить останню літеру зі строки
 function playGame() {
-  userInput = document.getElementById("cityInput");
-  if (userInput == "") {
-    alert(
-      "Введіть назву міста, яке закінчується на останню літеру міста, яке ввів користувач!"
-    );
-    return;
-  }
-  if (attempt < 0 || attempt > 3) {
-    alert("Неправильно! Спробуй ще раз!");
-  } else {
-    attempts = attempts++; // We increase the number of attempts by 1 - Кількість спроб збільшуємо на 1
-  }
-  if (attempts == 3) {
-    weFinished = true;
-    alert("Міста закінчились!");
-  }
-}
+  var userCityInput = document.getElementById("cityInput"); // звертаємось через об'єкт до елементу cityInput - користувач вводить місто в інпут
+  var outputElement = document.getElementById("output"); // output - виводить текстові повідомлення
+  var userCity = userCityInput.value.trim(); // userCityInput.value - повертає назву, введену користувачем і trim - видаляє пробіли
+  // Користувач вводить місто і йде перевірка
+  if (userCity.length > 0) {
+    var lastletter = userCity.charAt(userCity.length - 1); // отримаємо останню літеру введеного користувачем міста
+    var nextCity = getNextCity(lastletter);
+    // Перевірка чи існує наступне місто
+    if (nextCity) {
+      outputElement.textContent = "Cайт:" + nextCity; // встановлює текстовий вміст і виводиться повідомлення
+    } else {
+      // ще - перевірка кількості неправильних спроб та закінчення міст
+      wrongAttempts++; // We increase the number of attempts by 1 - Кількість спроб збільшуємо на 1
+      outputElement.textContent = "Будь ласка, введіть назву міста.";
+    }
 
-// 1. User input of a letter in a dialog box - Введення користувачем літери в діалогове вікно
-function playCityGame() {
-  userInput = prompt("Введіть літеру:");
+    userCityInput.value = "";
+    userCityInput.focus(); // для введення наступного міста
+  }
+  //  Search for a city that starts with a given letter - Пошук міста в пам'яті - викликаємо
+  // функцію наступного міста з параметром (остання літера)
+  function getNextCity(lastletter) {
+    for (var i = 0; i < cities.length; i++) {
+      // цикл пробігається по всіх містах, змінна i - початкове значення
+      var city = cities[i];
+      if (city.charAt(0) == lastletter && cities.indexOf(city) !== -1) {
+      }
+      // перевіряє чи перша літера імені міста співпадає з переданою літерою
+      // і перевіряє чи знаходиться місто в масиві
+      return city;
+    }
+    return null;
+  }
+  // перевіряє чи кількість неправильних спроб більше або дорівнює 3
+  if (wrongAttempts >= 3 || cities.length == 0) {
+    alert("Сайт: Я програв! Вітаю з перемогою!");
+  } else {
+    outputElement.textContent = "Сайт: Неправильно. Спробуй ще!";
+  }
 }
-// Check for correct letter input - Перевірка на правильність введення літери
-if (validateInput(userInput)) {
-  alert("Введіть, будь ласка лише одну букву.");
-}
-// 2. Remember the letter - Запам'ятати літеру
-var rememberLeter = userInput;
-// 3. Search for a city that starts with a given letter - Пошук міста, яке починається із заданої літери
-var matchesCity = findCity(rememberLeter);
-function findCity(letter) {
-  cities = ["Київ", "Вінниця", "Яготин", "Нова Каховка", "Авдіївка"];
-}
-// 4. Output of results - вивід результатів
-if (matchesCity) {
-  alert("Місто" + matchesCity + "Ви виграли!");
-} else {
-  alert("Міста закінчились, що починаються з цієї літери. Ви програли!");
+// Output of results - вивід результатів, функція означає чи виграв користувач або програв і виводь повідомлення
+function checkGameOver() {
+  var outputElement = document.getElementById("output");
+  outputElement.textContent = "Сайт: Я програв! Вітаю з перемогою!";
 }
